1e504db8303ed6b63b6051725e1ea84702de6b71
Francis <francis-r-emond@hotmail.com>
2019-01-26 11:45:48 -0500

    wiggle

--- Assets/Scripts/Mob.cs
+++ Assets/Scripts/Mob.cs
@@ -2,8 +2,33 @@
 
 public class Mob : MonoBehaviour
 {
+    void Awake()
+    {
+        m_Graphics = transform.Find("Graphics");
+    }
+
+    void Update()
+    {
+        if (m_Graphics != null)
+        {
+            m_GraphicsTimer += Time.deltaTime;
+
+            float sin = WiggleAmplitude * Mathf.Sin(WiggleSpeed * m_GraphicsTimer);
+            Quaternion quaternion = m_Graphics.rotation;
+            quaternion.eulerAngles = new Vector3(quaternion.eulerAngles.x, quaternion.eulerAngles.y, sin);
+            m_Graphics.rotation = quaternion;
+        }
+        
+    }
+
     public void OnPathComplete()
     {
         Destroy(gameObject);
     }
+
+    public float WiggleSpeed;
+    public float WiggleAmplitude;
+
+    private Transform m_Graphics;
+    private float m_GraphicsTimer;
 }
