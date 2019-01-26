eef9847125673eb06cd2b51d4ac9acdda887b28b
jan4tin <janick1992@hotmail.com>
2019-01-26 11:49:40 -0500

    tour cochon
    Add cochon, tower adn ressource classes

---
+++ Assets/Scripts/Tour.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TourType
+{
+    TourType_AOE,
+    TourType_Attack,
+    TourType_Static
+}
+
+public class Tour : MonoBehaviour
+{
+    public TourType m_TourType;
+    public Ressource m_Ressource;
+    public float m_MinBuildingTime;
+
+    private bool m_IsBeingBuild;
+    private bool m_IsBuilded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_IsBeingBuild = true;
+    }
+
+    void FinishBuilding()
+    {
+        m_IsBuilded = true;
+        m_IsBeingBuild = false;
+    }
+
+    public void Attack(uint damage)
+    {
+        uint hp = m_Ressource.Attack(damage);
+        if(hp <= 0) // pour l'instant
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public virtual void DoUpdate(float deltaTime) { }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_IsBuilded)
+        {
+            DoUpdate(Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/TourAOE.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TourAOE : Tour
+{
+    public float m_SpeedReduction;
+    public uint m_DamageOverTime;
+
+    private Mob m_OverlappingMob;
+    private bool m_MobIsOverlapping;
+
+    void Start()
+    {
+        m_MobIsOverlapping = false;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+    }
+
+    void OnCollisionExit(Collision collisionInfo)
+    {
+
+    }
+
+    public override void DoUpdate(float deltaTime)
+    {
+
+    }
+}
